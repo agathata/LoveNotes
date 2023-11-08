b8a8903ad882298ecd9e10055f81acde6246b602
August <tata@umich.edu>
2023-11-08 00:14:25 +0000

    add picture to admin interface

--- admin.py
+++ admin.py
@@ -2,7 +2,11 @@ from django.contrib import admin
 
 from .models import *
 
+class PictureAdmin(admin.ModelAdmin):
+    exclude = ("content", "content_type")
+
 admin.site.register(Chest)
 admin.site.register(Item)
 admin.site.register(Note)
+admin.site.register(Picture, PictureAdmin)
 admin.site.register(Settings)
